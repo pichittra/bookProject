7a7cb9c4ae21cab887f395f4b19f903dce4149c1
pichittra <pichittra.cch@gmail.com>
2017-10-05 11:26:10 +0700

    pipe truncate

--- src/app/truncate.ts
+++ src/app/truncate.ts
@@ -4,9 +4,15 @@ import {Pipe} from '@angular/core'
   name: 'truncate'
 })
 export class TruncatePipe {
-  transform(value: string, args: string[]) : string {
-     let limit = value.length > 0 ? parseInt(value, 8) : 8;
-     let trail = '...';
+  transform(value: string, args: string) : string {
+    //  let limit = value.length > 0 ? parseInt(value, 8) : 8;
+    //  let trail = '...';
+    let limit = args ? parseInt(args, 15) : 15;
+    let trail = '...';
+
+     console.log(value)
+     console.log(value.length)
+     console.log(args)
     //let limit = value.length > 1 ? value[1] : '...';
     return value.length > limit ? value.substring(0, limit) + trail : value;
   }
